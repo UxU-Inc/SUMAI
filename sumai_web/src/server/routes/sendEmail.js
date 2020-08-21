3e7181634e5cb041a6bfc4f76dcc5391f7ed43ce
smworld01 <smworld01@naver.com>
2020-08-21 21:32:06 +0900

    add email

--- sumai_web/src/server/routes/sendEmail.js
+++ sumai_web/src/server/routes/sendEmail.js
@@ -1,35 +1,30 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
+const gmailOAuth2Data   = require('../security/gmailOAuth2');
 const router = express.Router();
 
-router.post("/sendEmail", function(req, res, next){
+router.post("/sendEmail", function(req, res){
+  let transporter = nodemailer.createTransport(gmailOAuth2Data);
+  let email = req.body.email;
   let message = req.body.message;
 
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'uxu.co.kr@gmail.com',  // gmail 계정 아이디를 입력
-      pass: 'wjtmxmendlt!UXU'          // gmail 계정의 비밀번호를 입력
-    }
-  });
-
   let mailOptions = {
-    from: 'uxu.co.kr@gmail.com',    // 발송 메일 주소 (위에서 작성한 gmail 계정 아이디)
     to: 'uxu.co.kr@gmail.com' ,                     // 수신 메일 주소
-    subject: 'nodemailer test',   // 제목
+    subject: `의견 보내기 ${email!==undefined?email:''}`,   // 제목
     text: message  // 내용
   };
 
   transporter.sendMail(mailOptions, function(error, info){
     if (error) {
       console.log(error);
+      res.status(500).send()
     }
     else {
       console.log('Email sent: ' + info.response);
+      res.status(200).send()
     }
   });
 
-  res.redirect("/");
 })
 
 module.exports = router;
