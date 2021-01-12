b0ba8e7ae90a0eec1052525bcc64909124c84d98
smworld01 <smworld01@naver.com>
2021-01-12 17:39:59 +0900

    change cookie domain and change login after return url

--- sumai_web/src/components/AccountWithdrawal.js
+++ sumai_web/src/components/AccountWithdrawal.js
@@ -191,12 +191,17 @@ class AccountPassword extends React.Component {
 
                 this.props.logoutRequest().then(
                     () => {
+                        let domainIndex = window.location.hostname.indexOf('.') // ex) asdf.good.com -> 5 (.의 위치)
+                        let domainName
+                        if(domainIndex === -1) domainName = window.location.hostname // .을 못 찾은 경우 그대로 씀 -> localhost
+                        else domainName = window.location.hostname.substr(domainIndex) // .이 있는 경우 -> .good.com
+
                         // EMPTIES THE SESSION
                         let loginData = {
                             isLoggedIn: false,
                             email: ''
                         };
-                        document.cookie = 'key=' + btoa(JSON.stringify(loginData)) + ';path=/;';
+                        document.cookie = 'key=' + btoa(JSON.stringify(loginData)) + ';domain=' + domainName + ';path=/;';
                     }
                 );
 
